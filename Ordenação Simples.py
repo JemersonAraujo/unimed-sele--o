361ea012fee797c5427a0801e7b320a3d90a7087
JEMERSON <jemerson.araujo@hotmail.com>
2023-11-28 14:32:44 -0300

    Ordenação Simples

---
+++ Ordenação Simples.py
@@ -0,0 +1,11 @@
+def ordenar_lista(lista):
+    # Utiliza a função sorted para ordenar a lista em ordem crescente
+    lista_ordenada = sorted(lista)
+    return lista_ordenada
+
+# Exemplo de uso:
+numeros = [4, 2, 7, 1, 9, 3]
+lista_ordenada = ordenar_lista(numeros)
+
+# Imprime a lista ordenada
+print(lista_ordenada)
